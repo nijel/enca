20aa38b55209f1a139a43ab2b999471a7fc2c432
Michal Čihař <michal@cihar.com>
2010-02-09 11:00:54 +0100

    Adjust test for new reportbug url.

--- test/test-long-texts.sh
+++ test/test-long-texts.sh
@@ -9,5 +9,5 @@ $ENCA --license >$TESTNAME.actual || DIE=1
 diff $top_srcdir/COPYING $TESTNAME.actual || DIE=1
 $ENCA --help >$TESTNAME.actual || DIE=1
 diff $top_builddir/src/HELP $TESTNAME.actual || DIE=1
-grep '^Report bugs to .*<.*@.*>' $TESTNAME.actual >/dev/null || DIE=1
+grep '^Report bugs to .*http.*' $TESTNAME.actual >/dev/null || DIE=1
 . $srcdir/finish.sh
