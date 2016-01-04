db74ebfed3cbf827ad04c356734ba9cf8de8a1b5
Michal Čihař <michal@cihar.com>
2016-01-04 11:35:49 +0100

    TeX conversion needs recode
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test/test-tex.sh
+++ test/test-tex.sh
@@ -1,12 +1,16 @@
 #! /bin/sh
 # Purpose: test whether TeX decoding works.
 . $srcdir/setup.sh
-TEST_TEXT=$srcdir/cs-s.tex
-OPTS="-L cs"
-# File
-cp $TEST_TEXT $TESTNAME.actual
-$ENCA $OPTS -x UTF-8 $TESTNAME.actual || DIE=1
-$ENCA -L none $TESTNAME.actual | grep UTF-8 >/dev/null || DIE=1
-$ENCA $OPTS -x TEX $TESTNAME.actual || DIE=1
-diff $TEST_TEXT $TESTNAME.actual || DIE=1
+if $ENCA --list converters | grep '^librecode$' >/dev/null; then
+  TEST_TEXT=$srcdir/cs-s.tex
+  OPTS="-L cs -C recode"
+  # File
+  cp $TEST_TEXT $TESTNAME.actual
+  $ENCA $OPTS -x UTF-8 $TESTNAME.actual || DIE=1
+  $ENCA -L none $TESTNAME.actual | grep UTF-8 >/dev/null || DIE=1
+  $ENCA $OPTS -x TEX $TESTNAME.actual || DIE=1
+  diff $TEST_TEXT $TESTNAME.actual || DIE=1
+else
+  E77=1
+fi
 . $srcdir/finish.sh
