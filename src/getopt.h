c90a463a61433f78259f5ae92c106f6c18c9ac35
Syed-Shahrukh-OSSRevival <syed.shahrukh@ossrevival.org>
2025-05-27 00:17:58 +0500

    t59: Removed build error and warning for mingw gcc compiler on windowsi #59.
    Signed-off-by: Syed-Shahrukh-OSSRevival <syed.shahrukh@ossrevival.org>

--- src/getopt.h
+++ src/getopt.h
@@ -148,11 +148,11 @@ extern int getopt ();
 # endif /* __GNU_LIBRARY__ */
 
 # ifndef __need_getopt
-extern int getopt_long (int __argc, char *const *__argv, const char *__shortopts,
-		        const struct option *__longopts, int *__longind);
-extern int getopt_long_only (int __argc, char *const *__argv,
-			     const char *__shortopts,
-		             const struct option *__longopts, int *__longind);
+extern int getopt_long (int argc, char *const *argv, const char *shortopts,
+		        const struct option *longopts, int *longind);
+extern int getopt_long_only (int argc, char *const *argv,
+			     const char *shortopts,
+		             const struct option *longopts, int *longind);
 
 /* Internal only.  Users should not call this directly.  */
 extern int _getopt_internal (int __argc, char *const *__argv,
