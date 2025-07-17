8f6838a59bbd8e97eb793abefe9e0ac1f9bfd985
Syed-Shahrukh-OSSRevival <syed.shahrukh@ossrevival.org>
2025-07-17 21:38:37 +0500

    added fix for normalize.pl input format with  help to normalize.pl and countall #82

--- data/countall.c
+++ data/countall.c
@@ -1,15 +1,44 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 
 #define isbinary(x) (iscntrl(x) || (x) == 127)
 
 #define FILL_CHARACTER '.'
 
-int
-main(void)
+void print_usage(const char *progname) {
+    printf("Usage: %s < [SAMPLE_TEXT]\n", progname);
+    printf("Options:\n");
+    printf("  -h, --help            Show this help message\n");
+}
+
+int main(int argc, char *argv[])
 {
   unsigned long int count[0x100];
   int i, c;
+  int opt;
+  int option_index = 0;
+  static struct option long_options[] =
+    {
+    /* These options set a flag. */
+    {"help",     no_argument,       0, 'h'}
+    };
+
+  opt = getopt_long (argc, argv, "h:",
+                long_options, &option_index);
+
+  switch (opt) {
+      case -1:
+          break;
+      case 'h':
+          print_usage(argv[0]);
+          return 0;
+      default:
+          print_usage(argv[0]);
+          return 1;
+  }
 
   for (i = 0; i < 0x100; i++)
     count[i]=0;
--- data/normalize.pl
+++ data/normalize.pl
@@ -1,15 +1,25 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+use Getopt::Long;
+use Pod::Usage;
 
 my $n = 0;
 my $q;
 my $max = 0;
-my (@char, @count);
+my (@char_hex, @char, @count);
+my $help;
+my $man;
+my $no_zero;
+
+GetOptions("no-zero" => \$no_zero, 'help|?' => \$help, man => \$man) or pod2usage(2);
+pod2usage(1) if $help;
+pod2usage(-exitval => 0, -verbose => 2) if $man;
+pod2usage("$0: No files given.")  if ((@ARGV == 0) && (-t STDIN));
 
 if (!defined $ARGV[0]) {
   while (<STDIN>) {
-    ($char[$n], $count[$n]) = split /\s+/, $_, 2;
+    ($char_hex[$n], $char[$n], $count[$n]) = split /\s+/, $_, 3;
 
     if ($max < $count[$n]) {
       $max = $count[$n];
@@ -20,6 +30,9 @@ if (!defined $ARGV[0]) {
   for ($q = 1; $max/$q >= 6000; $q++) { }
 
   for (my $i = 0; $i < $n; $i++) {
+    if ($no_zero && ($count[$i]/$q) < 1) {
+      next;
+    }
     printf "%s %u\n", $char[$i], $count[$i]/$q;
   }
 }
@@ -36,7 +49,7 @@ else {
 
   my $sum2 = 0;
   while (<STDIN>) {
-    ($char[$n], $count[$n]) = split /\s+/, $_, 2;
+    ($char_hex[$n], $char[$n], $count[$n]) = split /\s+/, $_, 3;
 
     $sum2 += $count[$n];
     $n++;
@@ -64,6 +77,45 @@ else {
   }
 
   for (my $i = 0; $i < $n; $i++) {
+    if ($no_zero && ($count[$i]) < 1) {
+      next;
+    }
     printf "%s %u\n", $char[$i], $count[$i];
   }
 }
+
+__END__
+=head1  normalize.pl
+
+normalize.pl - Useful for producing CHARSET.base files, since the weights must fit
+               into unsigned short int:
+
+=head1 SYNOPSIS
+
+normalize.pl [options] [EXISTING_COUNT] < [RAW_COUNT]
+
+ Options:
+   -help            brief help message
+   -no-zero         Skip characters with zero counts
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-help>
+
+Print a brief help message and exits.
+
+=item B<-man>
+
+Prints the manual page and exits.
+
+=back
+
+=head1 DESCRIPTION
+
+B<This program> will read the given input file(s) and do something
+useful with the contents thereof.
+
+=cut
+
